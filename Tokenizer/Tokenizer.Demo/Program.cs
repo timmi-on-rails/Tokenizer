1d796a1738b07e1392a99f9fd4807ea823c0d5b9
Tom Scheler <dd-tom@web.de>
2017-06-14 00:50:46 +0200

    improved demo

--- Tokenizer/Tokenizer.Demo/Program.cs
+++ Tokenizer/Tokenizer.Demo/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using Tokenizer;
 
 namespace FileTokenizer
@@ -10,25 +8,29 @@ namespace FileTokenizer
 	{
 		public static void Main(string[] args)
 		{
-			int totalUnknown = 0;
-			int numberOfFiles = 0;
-
-			foreach (string fileName in Directory.EnumerateFiles(args[0], "*.cs", SearchOption.AllDirectories))
+			if (args.Length == 0 || string.IsNullOrWhiteSpace(args[0]))
 			{
-				IEnumerable<Token> tokens = Tokenize.FromFile(fileName);
-
-				IEnumerable<Token> uTokens = tokens.Where(token => token.TokenType == TokenType.Unknown).ToList();
-				if (uTokens.Count() > 0)
-				{
-					Console.WriteLine("Found {0} unknown tokens in file {1}: {2},...", uTokens.Count(), fileName,
-								  string.Join(",", uTokens.Take(5).Select(token => token.Content)));
-				}
-				totalUnknown += uTokens.Count();
-
-				numberOfFiles++;
+				Console.WriteLine("usage: TokenizeFile <file name>");
+				return;
 			}
 
-			Console.WriteLine("Total unknown tokens: {0}, in {1} files", totalUnknown, numberOfFiles);
+			string fileName = args[0];
+
+			if (!File.Exists(fileName))
+			{
+				Console.WriteLine(fileName + ": no such file");
+				return;
+			}
+
+			foreach (Token token in Tokenize.FromFile(fileName))
+			{
+				string escapedContent = token.Content
+											 .Replace("\r\n", @"\r\n")
+											 .Replace("\n", @"\n")
+											 .Replace("\t", @"\t");
+
+				Console.WriteLine("{0,7} {1,-17} {2}", token.Position, token.TokenType, escapedContent);
+			}
 		}
 	}
 }
